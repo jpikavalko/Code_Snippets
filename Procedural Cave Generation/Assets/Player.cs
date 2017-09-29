68268683a5d5cab042eef219fdc27f29a230ed94
jpikavalko <jpikavalko@gmail.com>
2017-09-29 18:01:00 +0300

    video 9

---
+++ Procedural Cave Generation/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody rb;
+    Vector3 velocity;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+	}
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime *10);
+    }
+}
---
+++ Procedural Cave Generation/Assets/Player2D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2D : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Vector2 velocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime * 10);
+    }
+}
