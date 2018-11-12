5a2b725c1a4429e2e7a0580342ec6693c3fd167c
jukka <jpikavalko@gmail.com>
2018-11-12 07:00:04 +0200

    L-system started

---
+++ L-System/Assets/Scripts/ConsoleLSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// axiom = A
+/// rules(A -> AB), (B -> A)
+/// </summary>
+public class ConsoleLSystem : MonoBehaviour {
+
+    protected string axiom = "A";
+    protected string sentence;
+
+    Dictionary<char, string> Rule = new Dictionary<char, string>();
+
+    public virtual void Start()
+    {
+        sentence = axiom;
+        Rule.Add('A', "AB");
+        Rule.Add('B', "A");
+        Generate();
+    }
+
+    protected void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Generate();
+        }
+    }
+
+    public virtual void Generate()
+    {
+        string nextSentence = "";
+
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            string current = sentence[i].ToString();
+            if (current == "A")
+                nextSentence += Rule['A'];
+            else if (current == "B")
+                nextSentence += Rule['B'];
+            else
+                nextSentence += current;
+        }
+
+        sentence = nextSentence;
+        Debug.Log(sentence);
+    }
+}
