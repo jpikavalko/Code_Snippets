d37d1a408303f2da3f8f4400b3968d9ec407d704
jpikavalko <jpikavalko@gmail.com>
2019-09-11 09:29:14 +0300

    Deleted Procedural Landmass Generation project
    - to start it again in different project

---
+++ Mirror_network_test/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerControl : NetworkBehaviour
+{
+    public float movementSpeed = 10f;
+
+    // Update Controls
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(horizontal, 0f, vertical) * movementSpeed * Time.deltaTime;
+    }
+}
